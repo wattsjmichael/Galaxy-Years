e5bd97d27354af8faa9fb017b1e0748c4721c5ac
Michael Watts <wattsjmichael@gmail.com>
2020-09-04 12:25:29 -0700

    figuring out the best way to run these tests

--- _tests_/galaxyyear.test.js
+++ _tests_/galaxyyear.test.js
@@ -1,15 +1,11 @@
-import  { GalaxyYear } from "./../src/galaxyyear.js"
+import  GalaxyYear from "./../src/galaxyyear.js"
 
 describe ('GalaxyYear', ()=> {
   test('should return earth year age', () =>{
     let earthAge = 20;
     expect(earthAge).toEqual(20);
-
-});
-  test("should return 41.68 Mercury Years if your 10 Earth Years old", () =>{
-    let earthAge = 10
-    earthAge.mercuryYears();
-    expect(mercuryOld.mercuryYears(10)).toBe(41.68);
-});
-
+  });
+  test ('Should return your Earth age of 10 years in 41.68 Mercury Years', ()=>{
+    expect(mercuryAge).toEqual(41.68);
+  });
 });
