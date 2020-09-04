37ef78344b9293dbd82c6318b8fd748f223df1be
Michael Watts <wattsjmichael@gmail.com>
2020-09-04 13:12:08 -0700

    still returing not a constructor, more research is needed

--- _tests_/galaxyyear.test.js
+++ _tests_/galaxyyear.test.js
@@ -3,7 +3,7 @@ import  GalaxyYear from "./../src/galaxyyear.js"
 describe ('GalaxyYear', ()=> {
   
   test('should return earth year age', () =>{
-    const whatevers = new GalaxyYear(20);
+    const whatevers = new GalaxyYear(20);asdf
     expect(whatevers.earthAge).toEqual(20);
   });
   test ('Should return your Earth age of 10 years in 41.68 Mercury Years', ()=>{
