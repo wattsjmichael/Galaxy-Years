64dc0985a917bf1f9ebde2d99c1c8413861ca832
Michael Watts <wattsjmichael@gmail.com>
2020-09-04 13:35:19 -0700

    Finally a meaningful pass!, onto Mercury Age

--- _tests_/galaxyyear.test.js
+++ _tests_/galaxyyear.test.js
@@ -1,13 +1,15 @@
-import  GalaxyYear from "./../src/galaxyyear.js"
+import  GalacticAge from "./../src/galaxyyear.js"
 
 describe ('GalaxyYear', ()=> {
   
   test('should return earth year age', () =>{
-    const whatevers = new GalaxyYear(20);asdf
-    expect(whatevers.earthAge).toEqual(20);
-  });
-  test ('Should return your Earth age of 10 years in 41.68 Mercury Years', ()=>{
-    const earthAge = new GalaxyYear(10); 
-    expect(earthAge.mercuryAge()).toEqual(41.68);
-  });
+    let galacticAge = new GalacticAge(10);
+    expect(galacticAge.earthAge).toEqual(10);
+    console.log(galacticAge.earthAge);
+    
+  // test ('Should return your Earth age of 10 years in 41.68 Mercury Years', ()=>{
+  //   const earthAge = new GalaxyYear(10); 
+  //   expect(earthAge.mercuryAge()).toEqual(41.68);
+  // });
+});
 });
