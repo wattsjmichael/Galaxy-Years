bee47d12d43cede38b925280c0461e5b0bc595bd
Michael Watts <wattsjmichael@gmail.com>
2020-09-04 10:14:12 -0700

    running tests

---
+++ _tests_/galaxyyear.test.js
@@ -0,0 +1,11 @@
+import { GalaxyYear } from "./../src/galaxyyear.js"
+
+describe ('Year', () => {
+
+  test('should return earth year age', () =>{
+    let age = new Age (20);
+  
+  
+  
+  });
+});
